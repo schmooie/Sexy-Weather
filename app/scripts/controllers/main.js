45cd631d82b1a0496d6d0d7e69fba239747c0c5a
Sam Lau <slau234@gmail.com>
2014-07-24 22:30:33 -0400

    Added user feedback for when a city isn't found.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('sexyWeather')
-  .controller('MainCtrl', ['$scope', '$http', '$q', 'utilities',
-    function($scope, $http, $q, utilities) {
+  .controller('MainCtrl', ['$scope', '$http', '$q', '$timeout', 'utilities',
+    function($scope, $http, $q, $timeout, utilities) {
       $scope.whens = [
         'today',
         'tomorrow',
@@ -13,41 +13,49 @@ angular.module('sexyWeather')
         when: 'today',
         where: 'New York'
       };
-
       $scope.subline = 'For example: <em>New York</em> or <em>River Edge</em>';
       $scope.isSexy = 0;
+      $scope.cityNotFound = false;
       $scope.sexyVideo = null;
 
       var fetchWeather = function(url, days) {
         var res = [];
         var deferred = $q.defer();
         $http.get(url).success(function(data) {
-          if (days > 1) {
-            data.list.forEach(function(el) {
-              res.push({
-                min: utilities.kToF(el.temp.min),
-                max: utilities.kToF(el.temp.max)
-              });
-            });
+          if (data.cod === '404') {
+          	deferred.reject(data.message);
           } else {
-            res.push({
-              min: utilities.kToF(data.main.temp_min),
-              max: utilities.kToF(data.main.temp_max),
-              now: utilities.kToF(data.main.temp)
-            });
+            if (days > 1) {
+              data.list.forEach(function(el) {
+                res.push({
+                  min: utilities.kToF(el.temp.min),
+                  max: utilities.kToF(el.temp.max)
+                });
+              });
+            } else {
+              res.push({
+                min: utilities.kToF(data.main.temp_min),
+                max: utilities.kToF(data.main.temp_max),
+                now: utilities.kToF(data.main.temp)
+              });
+            }
+            deferred.resolve(res);
           }
-          deferred.resolve(res);
-        });
+
+        })
+          .error(function(error) {
+            console.log(error);
+          });
         return deferred.promise;
       };
 
       var makeEndPoint = function(daily, location) {
         var endpoint = 'http://api.openweathermap.org/data/2.5/',
-          apiKey = '&APPID=e464e0c345e366e776a6e248c5428112';
+          secret = '&APPID=e464e0c345e366e776a6e248c5428112';
         if (daily) {
-          return endpoint + 'forecast/daily?q=' + location + '&cnt=7&mode=json' + apiKey;
+          return endpoint + 'forecast/daily?q=' + location + '&cnt=7&mode=json' + secret;
         } else {
-          return endpoint + 'weather?q=' + location + apiKey;
+          return endpoint + 'weather?q=' + location + secret;
         }
       };
 
@@ -57,7 +65,7 @@ angular.module('sexyWeather')
       };
 
       $scope.getWeather = function() {
-      	utilities.startSpinner();
+        utilities.startSpinner();
         var response = $scope.response,
           finalEndPoint = '',
           days = 0;
@@ -76,26 +84,34 @@ angular.module('sexyWeather')
 
         fetchWeather(finalEndPoint, days)
           .then(function(weatherArr) {
-          	utilities.stopSpinner();
-            if (days === 2) {
-              if (utilities.inRange(weatherArr[0].min, weatherArr[0].max, 69)) {
-                $scope.isSexy = 2;
-                $scope.sexyVideo = '39YUXIKrOFk';
-              } else {
-              	$scope.isSexy = 1;
-              }
-            } else {
-              weatherArr.forEach(function(el) {
-                if (utilities.inRange(el.min, el.max, 69)) {
+              utilities.stopSpinner();
+              if (days === 2) {
+                if (utilities.inRange(weatherArr[0].min, weatherArr[0].max, 69)) {
                   $scope.isSexy = 2;
                   $scope.sexyVideo = '39YUXIKrOFk';
+                } else {
+                  $scope.isSexy = 1;
                 }
-              });
-              if ($scope.isSexy !== 2) {
-                $scope.isSexy = 1;
+              } else {
+                weatherArr.forEach(function(el) {
+                  if (utilities.inRange(el.min, el.max, 69)) {
+                    $scope.isSexy = 2;
+                    $scope.sexyVideo = '39YUXIKrOFk';
+                  }
+                });
+                if ($scope.isSexy !== 2) {
+                  $scope.isSexy = 1;
+                }
               }
-            }
-          });
+            },
+            function(error) {
+            	utilities.stopSpinner();
+              console.log(error);
+              $scope.cityNotFound = true;
+              $timeout(function(){
+              	$scope.cityNotFound = false;
+              }, 2000);
+            });
       };
     }
   ]);
